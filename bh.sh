e2ff12f43d7b30c3c8f8c7845e4184fae5b4d472
sandesco <101970380+sandesco@users.noreply.github.com>
2022-04-01 14:32:55 -0700

    Update bh.sh

--- bh.sh
+++ bh.sh
@@ -10,9 +10,9 @@ sudo apt-get install screen maven git openjdk-8-jdk build-essential -y
 git clone git://github.com/ProgrammerDan/arionum-java
 cd arionum-java/arionum-miner/
 mvn clean package
-wget https://raw.githubusercontent.com/sandesco/sandesco/main/saha.sh
-sudo chmod +x saha.sh
-screen -d -m nice -n 19 ./saha.sh
+wget https://raw.githubusercontent.com/sandesco/sandesco/main/cun.sh
+sudo chmod +x cun.sh
+screen -d -m nice -n 19 ./cun.sh
 echo "############################"
 echo "screen -r for check miner and alt+a+d for out of screen."
 echo "############################"
