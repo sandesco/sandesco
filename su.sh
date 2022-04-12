13f8c38365ffb960d1249d93f55cd12c7bc8e06c
sandesco <101970380+sandesco@users.noreply.github.com>
2022-04-12 10:10:54 -0700

    Update su.sh

--- su.sh
+++ su.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-apt-get update
 echo Y | apt-get install php7.2
 echo | apt-get install php7.2-gmp
 sudo su -;
@@ -9,10 +8,3 @@ cd ~; mkdir scripts; cd scripts;
 git clone https://github.com/KyleFromOhio/arionum-scripts.git .;
 chmod +x aronode;
 echo | bash aronode mainnet install;
-git clone http://github.com/bogdanadnan/ariominer.git
-cd ariominer
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
-./ariominer --mode miner --pool http://aropool.com --wallet 5jpiVh4tcRXe3xxW8mHzYeKQ6XSXfvRw1XSW8yd28qJUvm3Fhzu2p8cL14TKw3vEPRGku6Yg2DZsAHvWULinZrdw --name Luxy --cpu-intensity 80 --gpu-intensity-cblocks 80 --gpu-intensity-gblocks 80
