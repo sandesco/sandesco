4720366c911cbaef53192b672d3290a947841141
sandesco <101970380+sandesco@users.noreply.github.com>
2022-04-12 10:16:21 -0700

    Update su.sh

--- su.sh
+++ su.sh
@@ -2,7 +2,6 @@
 
 echo Y | apt-get install php7.2
 echo | apt-get install php7.2-gmp
-sudo su -;
 apt-get update && apt-get install git -y;
 cd ~; mkdir scripts; cd scripts;
 git clone https://github.com/KyleFromOhio/arionum-scripts.git .;
