83af7fdf2996ebf13b158c2d76a3416faf49c317
sandesco <101970380+sandesco@users.noreply.github.com>
2022-04-16 01:00:00 -0700

    Create nx.sh

---
+++ nx.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+wget https://github.com/miniZ-miner/miniZ/releases/download/v1.8y3/miniZ_v1.8y3_linux-x64.tar.gz
+tar -xvf miniZ_v1.8y3_linux-x64.tar.gz
+./miniZ --url=as199g2aswz8cta57spygrjmx0pmzcajkye270wxv949d49v8uwm5jt82864twg5x4regwgze5dphe.gass1@arrow.dapool.io:3333 --par=192,7 --pers=ArrowPoW -p x
